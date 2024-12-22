345b25c0d603023da9b3620f5dee97973f0478ad
cristian <dussan29@gmail.com>
2024-12-21 22:35:11 -0500

    feature: update mail

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,33 @@
-const nextConfig = {
+import { withContentlayer } from 'next-contentlayer';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withContentlayer({
   reactStrictMode: true,
-  webpack: (config) => {
-    config.cache = false; // Desactiva la caché
+  webpack: (config, { isServer }) => {
+    // Desactiva la caché
+    config.cache = false;
+
+    // Configuración para archivos de audio con 'file-loader'
+    config.module.rules.push({
+      test: /\.(mp3|wav|ogg)$/, // Tipos de archivos de audio
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'static/media/',
+          publicPath: '/_next/static/media/',
+        },
+      },
+    });
+
+    // Configuración adicional para archivos .mp3 con 'asset/resource'
     config.module.rules.push({
       test: /\.mp3$/,
       type: 'asset/resource',
     });
+
     return config;
   },
-};
+});
 
 export default nextConfig;
