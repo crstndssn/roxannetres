e9bcccc1482055f003b29190c065b3661bf9b5c0
cristian <dussan29@gmail.com>
2024-12-21 20:50:18 -0500

    feature: update content

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 const nextConfig = {
-  reactStrictMode: true,// Mantén reactStrictMode habilitado para verificar buenas prácticas
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      type: 'asset/resource', // Manejo de archivos .mp3 como recursos
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
